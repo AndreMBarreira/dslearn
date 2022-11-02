3317680527679bb120be86729c046ccba1a45608
AndreMBarreira <andremilla.am@gmail.com>
2022-11-02 14:52:32 -0300

    User Entity, Seeding User Role

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Role.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Role.java
@@ -3,9 +3,19 @@ package com.devsuperior.dslearnbds.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
 public class Role implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String authority;
 	
@@ -14,7 +24,6 @@ public class Role implements Serializable{
 	}
 
 	public Role(Long id, String authority) {
-		super();
 		this.id = id;
 		this.authority = authority;
 	}
